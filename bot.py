f59aca73c12122236ead0b8b89a54dcffad6652e
nkhramtsov <nkramtsov@gmail.com>
2022-08-04 22:49:24 +0500

    Publish phot to Telegram channel

--- bot.py
+++ bot.py
@@ -6,5 +6,8 @@ if __name__ == '__main__':
     load_dotenv()
     chat_id = os.getenv('TELEGRAM_CHAT_ID')
     token = os.getenv('TELEGRAM_BOT_TOKEN')
+
     bot = telegram.Bot(token=token)
-    bot.send_message(chat_id=chat_id, text='This message was sent by python-telegram-bot')
+
+    caption = 'This photo provided by bot for educational purposes'
+    bot.send_photo(chat_id=chat_id, photo=open('images/epic_1.png', 'rb'), caption=caption)
